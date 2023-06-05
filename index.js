f041dbc2f8a14611bffa3fecf10724e7e01ee78a
tasmiakhaled <tasmiakhaled.csecu@gmail.com>
2023-06-05 14:42:26 +0600

    commenting and upload image done

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const http = require('http');
 const socketIo = require('socket.io');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const bodyParser = require('body-parser')
+const multer = require('multer');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -12,57 +13,98 @@ const io = socketIo(server);
 
 const port = process.env.PORT || 5000;
 
-//middle wares
 app.use(cors());
 app.use(express.json());
 
-// console.log(process.env.DB_USER);
-// console.log(process.env.DB_PASSWORD);
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lqoavff.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// File upload configuration using Multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        const fileName = Date.now() + '-' + file.originalname;
+        cb(null, fileName);
+    },
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid file type. Only image files are allowed.'), false);
+    }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 async function run() {
     try {
-        const productCollection = client.db('foodMood').collection('products');
-        const userCollection = client.db('foodMood').collection('users');
+        await client.connect();
+        console.log('Connected to MongoDB');
+
+        const db = client.db('foodMood');
+        const productCollection = db.collection('products');
+        const userCollection = db.collection('users');
+        const reviewCollection = db.collection('reviews');
 
         app.get('/foods', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
-        })
+        });
 
         app.get('/foods/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const product = await productCollection.findOne(query);
             res.send(product);
-        })
+        });
 
         app.post('/addUser', async (req, res) => {
             const user = req.body;
-            userCollection.insertOne(user)
-                .then(result => {
-                    res.send(result.insertedCount > 0)
-                })
-        })
+            const result = await userCollection.insertOne(user);
+            res.send(result.insertedCount > 0);
+        });
 
         app.get('/users', async (req, res) => {
             const query = {};
             const cursor = userCollection.find(query);
             const users = await cursor.toArray();
             res.send(users);
-        })
+        });
+
+        app.post('/addReview', upload.single('image'), async (req, res) => {
+            const { name, email, review } = req.body;
+            const image = req.file ? req.file.path : null;
+
+            const reviewObj = {
+                name,
+                email,
+                review,
+                image,
+            };
+
+            const result = await reviewCollection.insertOne(reviewObj);
+            res.send(result.insertedCount > 0);
+        });
+
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
 
         app.get('/users/:userName', async (req, res) => {
             const userName = req.params.userName;
             const query = { userName: userName };
             const user = await userCollection.findOne(query);
             res.send(user);
-        })
+        });
 
         app.put('/foods/:id/like', async (req, res) => {
             const id = req.params.id;
@@ -89,10 +131,8 @@ async function run() {
                 res.status(500).send({ error: 'An error occurred while updating the dislike count.' });
             }
         });
-    }
-
-    finally {
-
+    } finally {
+        // Perform cleanup tasks if required
     }
 }
 
@@ -101,7 +141,6 @@ run().catch(err => console.error(err));
 io.on('connection', (socket) => {
     console.log(`Socket ${socket.id} connected`);
 
-    // Handle the "check username" event
     socket.on('check-userName', async (userName) => {
         const users = client.db('foodMood').collection('users');
         const user = await users.findOne({ userName });
@@ -113,16 +152,17 @@ io.on('connection', (socket) => {
         }
     });
 
-    // Handle the "disconnect" event
     socket.on('disconnect', () => {
         console.log(`Socket ${socket.id} disconnected`);
     });
 });
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 app.get('/', (req, res) => {
     res.send('food server is running');
-})
+});
 
 server.listen(port, () => {
     console.log(`Food server is running on ${port}`);
-})
+});
